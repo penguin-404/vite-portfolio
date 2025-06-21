833c41aee61cfe6a0ec3cdc2885c25378069dc41
Kaushal <077bct020.kaushal@sagarmatha.edu.np>
2025-06-21 08:16:19 +0545

    Minor changes with mail address added to the contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,48 +1,39 @@
-import React from 'react'
+import React from 'react';
 
 export default function Contact() {
-    return (
-        <div className="relative flex items-top justify-center min-h-[560px] bg-white dark:bg-gray-800 sm:items-center sm:pt-0">
-            <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
-                <div className="mt-8 overflow-hidden">
-                    <div className="grid grid-cols-1 md:grid-cols-2">
-                        <div className="p-6 mr-2 bg-gray-100 sm:rounded-lg">
-                            <h1 className="text-3xl sm:text-4xl text-gray-800 font-extrabold tracking-tight">
-                                Get in touch: 
-                            </h1>
-                            <p className="text-normal text-lg sm:text-xl font-medium text-gray-600 mt-2">
-                                Fill in the form to start a conversation
-                            </p>
+  return (
+    <div className="relative flex items-top justify-center min-h-[560px] bg-white dark:bg-gray-800 sm:items-center sm:pt-0">
+      <div className="max-w-6xl mx-auto sm:px-6 lg:px-8">
+        <div className="mt-8 overflow-hidden">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            {/* Left Side */}
+            <div className="p-6 mr-2 bg-gray-100 dark:bg-gray-900 sm:rounded-lg">
+              <h1 className="text-3xl sm:text-4xl text-gray-800 dark:text-white font-extrabold tracking-tight">
+                Get in touch:
+              </h1>
+              <p className="text-lg sm:text-xl font-medium text-gray-600 dark:text-gray-300 mt-2">
+                Prefer email? I'm just a click away.
+              </p>
 
-                            <div className="flex items-center mt-8 text-gray-600">
-                                <svg
-                                    fill="none"
-                                    stroke="currentColor"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="1.5"
-                                    viewBox="0 0 24 24"
-                                    className="w-8 h-8 text-gray-500"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="1.5"
-                                        d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                                    />
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="1.5"
-                                        d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                                    />
-                                </svg>
-                                <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    Kathmandu, Nepal
-                                </div>
-                            </div>
+              <div className="flex items-center mt-8 text-gray-600 dark:text-gray-300">
+                <svg
+                  className="w-8 h-8 text-gray-500 dark:text-orange-400"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M17.657 16.657L13.414 20.9a2 2 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
+                  <path d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
+                </svg>
+                <div className="ml-4 text-md tracking-wide font-semibold w-40">
+                  Kathmandu, Nepal
+                </div>
+              </div>
 
-                            {/* <div className="flex items-center mt-4 text-gray-600">
+              {/* <div className="flex items-center mt-4 text-gray-600">
                                 <svg
                                     fill="none"
                                     stroke="currentColor"
@@ -64,79 +55,36 @@ export default function Contact() {
                                 </div>
                             </div> */}
 
-                            <div className="flex items-center mt-2 text-gray-600">
-                                <svg
-                                    fill="none"
-                                    stroke="currentColor"
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    stroke-width="1.5"
-                                    viewBox="0 0 24 24"
-                                    className="w-8 h-8 text-gray-500"
-                                >
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="1.5"
-                                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                                    />
-                                </svg>
-                                <div className="ml-4 text-md tracking-wide font-semibold w-40">
-                                    madebykaushal@gmail.com
-                                </div>
-                            </div>
-                        </div>
-
-                        <form  className="p-6 flex flex-col justify-center">
-                            <div className="flex flex-col">
-                                <label for="name" className="hidden">
-                                    Full Name
-                                </label>
-                                <input
-                                    type="name"
-                                    name="name"
-                                    id="name"
-                                    placeholder="Full Name"
-                                    className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
-                                />
-                            </div>
-
-                            <div className="flex flex-col mt-2">
-                                <label for="email" className="hidden">
-                                    Email
-                                </label>
-                                <input
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    placeholder="Email"
-                                    className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
-                                />
-                            </div>
-
-                            <div className="flex flex-col mt-2">
-                                <label for="tel" className="hidden">
-                                    Number
-                                </label>
-                                <input
-                                    type="tel"
-                                    name="tel"
-                                    id="tel"
-                                    placeholder="Telephone Number"
-                                    className="w-100 mt-2 py-3 px-3 rounded-lg bg-white border border-gray-400 text-gray-800 font-semibold focus:border-orange-500 focus:outline-none"
-                                />
-                            </div>
-
-                            <button
-                                type="submit"
-                                className="md:w-32 bg-orange-700 hover:bg-blue-dark text-white font-bold py-3 px-6 rounded-lg mt-3 hover:bg-orange-600 transition ease-in-out duration-300"
-                            >
-                                Submit
-                            </button>
-                        </form>
-                    </div>
+              <div className="flex items-center mt-4 text-gray-600 dark:text-gray-300">
+                <svg
+                  className="w-8 h-8 text-gray-500 dark:text-orange-400"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                </svg>
+                <div className="ml-4 text-md tracking-wide font-semibold w-60">
+                  madebykaushal@gmail.com
                 </div>
+              </div>
             </div>
+
+            {/* Right Side (Minimal) */}
+            <div className="p-6 flex flex-col justify-center items-center bg-white dark:bg-gray-800">
+              <a
+                href="mailto:madebykaushal@gmail.com"
+                className="bg-orange-700 hover:bg-orange-600 text-white font-bold py-3 px-6 rounded-lg transition duration-300"
+              >
+                Send an Email →
+              </a>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
